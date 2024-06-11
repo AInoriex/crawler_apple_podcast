91c67c3bfd54245723c443cf6c6ec0a5dd98de73
AInoriex <xiangyuhao@52tt.com>
2024-06-11 11:26:42 +0800

    [feat] 补充部署脚本；

---
+++ deploy.bat
@@ -0,0 +1,9 @@
+git clone -b feature/dev_magic_database https://github.com/AInoriex/crawler_apple_podcast.git
+
+cd crawler_apple_podcast
+
+python -m venv .venv
+
+.venv\Scripts\activate
+
+pip install -r .\requirements.txt
