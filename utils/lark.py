d0535792e7e0ce4db3ad1a94fa0a3d2d5ef2b07e
AInoriex <alexinori@163.com>
2025-03-18 16:14:59 +0800

    [feat] 补充通知webhook兼容性

--- utils/lark.py
+++ utils/lark.py
@@ -1,4 +1,5 @@
 import requests
+import os
 import json
 
 # class LarkNotice():
@@ -12,21 +13,23 @@ def alarm_lark_text(webhook:str, text:str)->bool:
 	    "msg_type": "text",
 	    "content": {"text": "test hello world."}
     }'''
+    # try to get webhook from global env
+    webhook = os.getenv("LARK_WEBHOOK") if webhook == "" else webhook
+    if webhook == "":
+        print("[utils.lark] webhook is empty, skip alarm to lark.")
+        return False
     params = {
 	    "msg_type": "text",
 	    "content": {"text": f"{text}"}
     }
-    print(f"request: {webhook} | {params}")
+    print(f"[utils.lark] request: {webhook} | {params}")
     resp = requests.post(url=webhook, json=params)
-    print(f"response: {resp.status_code} {resp.content}")
+    # print(f"[utils.lark] response: {resp.status_code} {resp.content}")
     if resp.status_code != 200:
+        print(f"[utils.lark] alarm to lark failed, response: {resp.status_code} {resp.content}")
         return False
     resp = resp.json()
     if resp["code"] != 0:
+        print(f"[utils.lark] alarm to lark failed, json: {resp}")
         return False
     return True
-
-if __name__ == "__main__":
-    webhook = "https://open.feishu.cn/open-apis/bot/v2/hook/34755f1e-5fc7-46c9-9fee-177317a581ee"
-    text = "【%s】 \n告警信息:%s \n机器IP:%s \n详情:%s \n告警时间:%s"%("Crawler_Name", "测试通知", "127.0.0.1", "测试，忽略😶♻🏝💨💦🙏👀✨💬", "2024/05/27 17:36")
-    alarm_lark_text(webhook=webhook, text=text)
